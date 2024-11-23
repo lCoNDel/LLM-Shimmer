2f7773490696e6ddbf00e5871478b7d64ed02faa
Luis Conde. <63459428+xCoNDex@users.noreply.github.com>
2024-11-23 17:12:38 +0100

    Create 4o-mini.py

---
+++ benchmark/4o-mini.py
@@ -0,0 +1,22 @@
+# Asistente básico
+
+import openai
+import os
+
+openai.api_key = os.environ['OPENAI_API_KEY']
+
+def chat_with_gpt(prompt):
+    response = openai.chat.completions.create(
+        model="gpt-4o-mini", # código open ai para el bot
+        messages=[{"role": "user", "content": prompt}]
+    )
+    return response.choices[0].message.content.strip()
+
+if __name__ == "__main__":
+    while True:
+        user_input = input("You: ")
+        if user_input.lower() in ["quit", "exit", "bye"]:
+            break
+
+        response = chat_with_gpt(user_input)
+        print("Chatbot: ", response)
